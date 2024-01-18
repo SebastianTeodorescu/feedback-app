a0cd8a608b1d7a254fa79128ab3d3677553cca45
Teodorescu Sebastian <teodorescusebastian95gmail.com>
2024-01-18 19:33:48 +0200

    Feedback-app: -create a Context to pass data through the component tree without passing props manually at every level. -get global state with useContext. -moving functions to context. -adding an edit feedback event & update feedback item. -side effects with useEffect

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackForm({ handleAdd }) {
+function FeedbackForm() {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
 
   const handleTextChange = (e) => {
     if (text === "") {
@@ -31,8 +43,11 @@ function FeedbackForm({ handleAdd }) {
         text: text,
         rating: rating,
       };
-
-      handleAdd(newFeedback);
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
+      }
       setText("");
     }
   };
@@ -61,4 +76,3 @@ function FeedbackForm({ handleAdd }) {
 }
 
 export default FeedbackForm;
-
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,13 +1,15 @@
-import PropTypes from 'prop-types'
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
 
-function FeedbackStats({ feedback }) {
   let average =
     feedback.reduce((acc, cur) => {
       return acc + cur.rating;
-    }, 0) / feedback.length
+    }, 0) / feedback.length;
 
-    average = average.toFixed(1).replace(/[.,]0$/, '')
+  average = average.toFixed(1).replace(/[.,]0$/, "");
 
   return (
     <div className="feedback-stats">
@@ -15,10 +17,6 @@ function FeedbackStats({ feedback }) {
       <h4>Average Rating: {average ? average : 0}</h4>
     </div>
   );
-}
-
-FeedbackStats.propTypes = {
-    feedback: PropTypes.array.isRequired
 }
 
 export default FeedbackStats;
